9ce589cc4546849f588e935c1bef112fd41f8bfe
mahesh <maheshchari@gmail.com>
2023-01-31 23:04:19 +0000

    Code from Server

---
+++ scripts/createmongodb.js
@@ -0,0 +1,19 @@
+const MongoClient = require('mongodb').MongoClient;
+
+// Connect URL
+const url = 'mongodb://127.0.0.1:27017';
+
+// Connec to MongoDB
+MongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, client) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    // Specify database you want to access
+    const db = client.db('admin');
+
+    console.log(`MongoDB Connected: ${url}`);
+});
